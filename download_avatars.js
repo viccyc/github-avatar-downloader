5ef19d436511306bdbb8c560d9af2a6df346a4c4
Viccy Grace <viccy.c.grace@gmail.com>
2018-01-23 03:25:50 +0000

    modified download_avatars.js to parse object and iterate over results

--- download_avatars.js
+++ download_avatars.js
@@ -3,22 +3,30 @@ var secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
-    url: "https://api.github.com/repos" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'request'
+      'Authorization': secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+  // parse the JSON string into an object
+    var data = JSON.parse(body);
+  // pass this object to the callback function
+    callback(err, data);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  // iterate over the results and (for now) console.log the value
+  // for each avatar_url in the collection
+  result.forEach(function(objAvatar) {
+    console.log("---Person:", objAvatar);
+  });
 });
 
