b9b2adb7414ae422f47d11d0d09ee67dc319cae6
Viccy Grace <viccy.c.grace@gmail.com>
2018-01-23 05:00:38 +0000

    modified download_avatars.js so far... to be completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -20,13 +21,45 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .on('error', function (err) {                                   // Note 2
+           throw err;
+         })
+         .on('response', function (response) {                           // Note 3
+           console.log('Downloading image...');
+           console.log('Response Status Code: ', response.statusCode);
+           console.log('Response Message: ', response.statusMessage);
+           console.log('Response Content Type: ', response.headers['content-type']);
+         })
+         .on('end', function (response) {
+           console.log('Download complete.');
+         })
+         .pipe(fs.createWriteStream('./test.jpg'));               // Note 4
+ }
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
-  // iterate over the results and (for now) console.log the value
+  let loginAvatarUrl = {};
+  // iterate over the results
   // for each avatar_url in the collection
   result.forEach(function(objAvatar) {
-    console.log("---Person:", objAvatar);
+    loginAvatarUrl[objAvatar['login']] = objAvatar['avatar_url'];
   });
+  for (login in loginAvatarUrl) {
+    downloadImageByURL(loginAvatarUrl[login], './avatars/' + login + '.jpg');
+  }
 });
 
+
+// function handleFilter(err, result) {
+//  let parsed = JSON.parse(result);
+//  let loginAvatarUrl = {};
+//  //cretes {'login name': 'avatar_url'} object
+//  parsed.forEach(function(contributor) {
+//    loginAvatarUrl[contributor['login']] = contributor['avatar_url'];
+//  });
+//  for (login in loginAvatarUrl) {
+//    downloadImageByURL(loginAvatarUrl[login], './avatars/' + login + '.jpg');
+//  }
+// }
